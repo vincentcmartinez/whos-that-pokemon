e4a8e163603ed92b9c87f89c9bd82f508f43b49b
shahzaibhash <hashmi.sz@outlook.com>
2025-06-30 11:33:51 -0400

    initial singleplayer - api fetch, multiple pokemon loading, visibility toggle, and reset

--- app/(tabs)/game.tsx
+++ app/(tabs)/game.tsx
@@ -1,18 +1,98 @@
-import { Stack } from 'expo-router';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Stack, useFocusEffect, useRouter } from 'expo-router';
+import { useCallback, useState } from "react";
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+
+
+const fetchPokemon = async (id) => {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    return {id, name: data.name, spriteURL: data.sprites.front_default};
+
+  } catch (error) {
+    console.error("fetch", error);
+    
+  }
+}
+
 
 export default function GameScreen() {
+  const router = useRouter();
+
+  useFocusEffect(
+    useCallback(() => {
+      resetGame();
+    }, [])
+  );
+
+  const [gamePokemons, setGamePokemons] = useState([]);
+  const [currentPokemonIndex, setCurrentPokemonIndex] = useState(0);
+  const [spriteVisible, setSpriteVisible] = useState(false)
+
+
+  const resetGame = () => {
+    setGamePokemons([]);
+    setCurrentPokemonIndex(0);
+    setSpriteVisible(false);
+    loadPokemon();
+  }
+
+  const loadPokemon = async () => {
+    try {
+      const pokemonIDList = [1, 4, 7, 10, 11]
+      const promises = pokemonIDList.map(id => fetchPokemon(id));
+      const pokemons = await Promise.all(promises);
+
+      setGamePokemons(pokemons);
+
+    } catch (error) {
+      console.error("load", error);
+    }
+  }
+
+  
+  
+  const toggleSpriteVisible = () => {
+    setSpriteVisible(visibility => !visibility);
+  }
+
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{ title: "Game" }} />
+      <Stack.Screen options={{ title: "Game"}} />
       <Text style={styles.title}>Guess the Pokémon!</Text>
-      <TextInput placeholder="Your guess..." style={styles.input} />
+
+      
+
+      {gamePokemons[currentPokemonIndex]?.spriteURL && (<Image source = {{uri: gamePokemons[currentPokemonIndex].spriteURL}}
+        style = {spriteVisible ? styles.spriteRevealed : styles.spriteHidden} //filter
+      />)}
+
+      {/* 
+
+      // <TextInput placeholder="Your guess..." style={styles.input} />
+      
+      */}
+
+      <Pressable onPress = {() => setCurrentPokemonIndex(currentPokemonIndex + 1)}> 
+        <Text>Reload</Text>
+      </Pressable>
+
+      <Pressable onPress = {() => toggleSpriteVisible()}>
+        <Text>Toggle</Text>
+      </Pressable>
+
+      <Pressable onPress = {() => router.push("/")}>
+        <Text>Return</Text>
+      </Pressable>
+
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 },
   title: { fontSize: 22, marginBottom: 20 },
   input: { width: '100%', borderColor: '#aaa', borderWidth: 1, padding: 10, borderRadius: 5 },
+  spriteHidden: {width: 300, height: 300, tintColor: "black"},
+  spriteRevealed: {width: 300, height: 300}
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,7 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import { Stack, useRouter } from 'expo-router';
-import { View, Text, Button } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   const router = useRouter();
@@ -14,7 +9,7 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <Stack.Screen options= {{ title: "Home" }} />
       <Text style={styles.title}>Who's That Pokemon?</Text>
-      <Button title="Start Game" onPress={() => router.push('./game')} />
+      <Button title="Start Game" onPress={() => {router.push('./game')}} />
     </View>
   )
     
